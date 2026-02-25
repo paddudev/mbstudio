6e261199eefea8cb6fe1dffc43658d09b3432a68
pradeep <pradeep.a@outlook.com>
2026-02-25 14:46:33 +0530

    initial commit

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import {defineconfig} from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineconfig({
-    Plugin: [react()],
-    base: process.env.VITE_BASE_PATH || "/mdstudio"
-})
